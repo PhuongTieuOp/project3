af26332901367d584942bf49e0e333e9a50b392b
Berta Devenyi <berta.j.devenyi@gmail.com>
2022-09-11 15:13:59 +1000

    Updated team presentation after discussion

--- assets/js/policedata.js
+++ assets/js/policedata.js
@@ -32,7 +32,7 @@ function buildTable(data){
 
 // LGA into dropdown
 function dropdown_init() {
-    var dropdown = d3.select("#selDataset")
+    var dropdown = d3.select("#lga")
     d3.csv(data)
         .then( res => {
         res.forEach(
@@ -53,12 +53,12 @@ dropdown_init();
 function filterListener(){
     d3.event.preventDefault();
     let uyear = d3.select("#Year").property("value");
-    let ulga = d3.select("#selDataset").property("value");
+    let ulga = d3.select("#lga").property("value");
     console.log(uyear);
     console.log(ulga);
     // this will work if you provide all of the information
     filters["Year"] = uyear;
-    filters["selDataset"] = ulga;
+    filters["lga"] = ulga;
     console.log(filters);
     let filteredData = data;
 
@@ -77,9 +77,9 @@ function filterListener(){
             // filteredData = filteredData.filter((row) => row.Year === uyear);
         }
         if (ulga) {
-            console.log(Object.values(filteredData).filter((row) => row.selDataset === ulga));
-            filteredData = Object.values(filteredData).filter((row) => row.selDataset === ulga);
-//            filteredData = filteredData.filter((row) => row.selDataset === ulga);
+            console.log(Object.values(filteredData).filter((row) => row.lga === ulga));
+            filteredData = Object.values(filteredData).filter((row) => row.lga === ulga);
+//            filteredData = filteredData.filter((row) => row.lga === ulga);
 
         }
     
@@ -95,3 +95,33 @@ d3.selectAll("#filter-btn").on('click', filterListener);
 
 //Populate tableData from data.js and this will display on the webpage
 buildTable(data);
+
+
+function plotting(unique_id) {
+    d3.json(samples).then(function (data) {
+        var samples = data.samples;
+        var resultsArray = samples.filter(sampleObj => sampleObj.id == unique_id);
+        var results = resultsArray[0];
+        var otu_ids = results.otu_ids;
+        var otu_labels = results.otu_labels;
+        var sample_values = results.sample_values;
+        var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+
+        // Building the horizontal bar chart
+        var bar_trace = {
+            type: 'bar',
+            orientation: 'h',
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            text: otu_labels.slice(0, 10).reverse(),
+        };
+        var bar_data = [bar_trace];
+        var bar_title = {
+            title: "Top 10 OTU values for the selected individual",
+        };
+
+        // Plotting horizontal chart
+        Plotly.newPlot("bar", bar_data, bar_title);
+
+    });
+}
